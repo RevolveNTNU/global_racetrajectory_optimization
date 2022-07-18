f9a2874a00d32cd7502928deb1d7beb10eb5e29a
Aksel Næstby <47793896+akselerando@users.noreply.github.com>
2022-07-18 17:20:32 +0200

    remove docstring

--- helper_funcs_glob/src/export_traj_race.py
+++ helper_funcs_glob/src/export_traj_race.py
@@ -4,18 +4,6 @@ import csv
 
 def export_traj_race(file_paths: dict,
                      traj_race: np.ndarray) -> None:
-    """
-    Created by:
-    Alexander Heilmeier
-
-    Documentation:
-    This function is used to export the generated trajectory into a file. The generated files get an unique UUID and a
-    hash of the ggv diagram to be able to check it later.
-
-    Inputs:
-    file_paths:     paths for input and output files {ggv_file, traj_race_export, traj_ltpl_export, lts_export}
-    traj_race:      race trajectory [s, x, y, psi, k, vx, ax]
-    """
     # export race trajectory
     with open(file_paths["traj_race_export"], 'w') as file:
         writer = csv.writer(file, delimiter=",")
