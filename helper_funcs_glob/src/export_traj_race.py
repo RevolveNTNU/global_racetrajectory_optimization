5dcb11bca8dc8b16fc8bdbd978e0c57c4da28911
Aksel Næstby <47793896+akselerando@users.noreply.github.com>
2022-07-20 14:21:30 +0200

    Update helper_funcs_glob/src/export_traj_race.py
    Co-authored-by: Brage Imset <42784301+BrageI@users.noreply.github.com>

--- helper_funcs_glob/src/export_traj_race.py
+++ helper_funcs_glob/src/export_traj_race.py
@@ -6,7 +6,7 @@ def export_traj_race(file_paths: dict,
                      traj_race: np.ndarray) -> None:
     # export race trajectory
     with open(file_paths["traj_race_export"], 'w') as file:
-        # make heading reference be the same as the x direction of vehicle
+        # make heading reference be the same as the x direction of vehicle at its starting pose
         traj_race[:, 3] += np.pi/2
 
         writer = csv.writer(file, delimiter=",")
